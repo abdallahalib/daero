75c1b31a1d82568857a132fe4dc4ae9db37a325e
abdallahali <theabdallahali@gmail.com>
2026-06-03 19:54:25 +0300

    Add IssueUi data class and extension function to convert Issue to IssueUi

---
+++ app/src/main/java/com/example/daero/issue_list/presentation/model/IssueUi.kt
@@ -0,0 +1,32 @@
+package com.example.daero.issue_list.presentation.model
+
+import com.example.daero.issue_list.domain.model.Issue
+import com.example.daero.issue_list.presentation.util.toReadableDateTime
+
+data class IssueUi(
+    val id: String,
+    val photoPath: String,
+    val title: String,
+    val notes: String,
+    val location: String,
+    val priority: String,
+    val status: String,
+    val syncStatus: String,
+    val createdAt: String,
+    val updatedAt: String,
+)
+
+fun Issue.toUi(): IssueUi {
+    return IssueUi(
+        id = id,
+        photoPath = photoPath,
+        title = title,
+        notes = notes,
+        location = location,
+        priority = priority.toReadableString(),
+        status = status.toReadableString(),
+        syncStatus = syncStatus.toReadableString(),
+        createdAt = createdAt.toReadableDateTime(),
+        updatedAt = updatedAt.toReadableDateTime(),
+    )
+}
