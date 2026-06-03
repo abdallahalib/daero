38987c2762984d75fe1d2117302b6e44487267c9
abdallahali <theabdallahali@gmail.com>
2026-06-03 22:07:27 +0300

    Integrate IssueListScreen into MainActivity and set up ViewModel

--- app/src/main/java/com/example/daero/MainActivity.kt
+++ app/src/main/java/com/example/daero/MainActivity.kt
@@ -4,21 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.daero.issue_list.presentation.IssueListScreen
+import com.example.daero.issue_list.presentation.IssueListViewModel
 import com.example.daero.ui.theme.DaeroTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+    private val issueListViewModel: IssueListViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            val issueListUiState = issueListViewModel.uiState.collectAsStateWithLifecycle().value
+            DaeroTheme(darkTheme = true) {
+                IssueListScreen(issueListUiState = issueListUiState)
+            }
         }
     }
 }
