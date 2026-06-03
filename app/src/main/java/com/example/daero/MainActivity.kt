9ec75555b8c0a78c5c0fee6f61c7064dc11f4479
abdallahali <theabdallahali@gmail.com>
2026-06-03 15:12:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/daero/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.daero
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.daero.ui.theme.DaeroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
