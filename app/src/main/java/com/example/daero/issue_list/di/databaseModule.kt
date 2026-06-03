07112cda015e577ae23e0ed2abad2adbf42df4b4
abdallahali <theabdallahali@gmail.com>
2026-06-03 19:16:49 +0300

    Add application class and Koin database module for dependency injection

---
+++ app/src/main/java/com/example/daero/issue_list/di/databaseModule.kt
@@ -0,0 +1,18 @@
+package com.example.daero.issue_list.di
+
+import androidx.room.Room
+import com.example.daero.issue_list.data.local.db.AppDatabase
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val databaseModule = module {
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            AppDatabase::class.java,
+            "issue_database"
+        ).build()
+    }
+
+    single { get<AppDatabase>().issueDao() }
+}
