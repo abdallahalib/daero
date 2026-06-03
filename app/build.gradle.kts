1fd6f4b1937b711f8f219bcf7fab1188e6d807b8
abdallahali <theabdallahali@gmail.com>
2026-06-03 22:31:34 +0300

    Add camera feature support and dependencies in manifest and build files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,8 @@ dependencies {
     implementation(libs.koin.androidx.compose)
     testImplementation(libs.koin.test)
     implementation(libs.coil.compose)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.compose)
 }
