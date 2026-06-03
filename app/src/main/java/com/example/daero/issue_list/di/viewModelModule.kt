cf19f480e99c5c1a98dded55c6550a4576f056a7
abdallahali <theabdallahali@gmail.com>
2026-06-03 23:22:43 +0300

    Add NewIssueViewModel to dependency injection module

--- app/src/main/java/com/example/daero/issue_list/di/viewModelModule.kt
+++ app/src/main/java/com/example/daero/issue_list/di/viewModelModule.kt
@@ -1,11 +1,15 @@
 package com.example.daero.issue_list.di
 
 import com.example.daero.issue_list.presentation.IssueListViewModel
+import com.example.daero.new_issue.NewIssueViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel {
         IssueListViewModel(get())
+    }
+    viewModel {
+        NewIssueViewModel()
     }
 }
