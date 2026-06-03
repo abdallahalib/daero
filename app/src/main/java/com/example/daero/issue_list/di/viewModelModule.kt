ef5c350075e7a8fde7e0ff801759ecc517940156
abdallahali <theabdallahali@gmail.com>
2026-06-04 00:33:41 +0300

    Add AppStorage implementation to viewModelModule for dependency injection

--- app/src/main/java/com/example/daero/issue_list/di/viewModelModule.kt
+++ app/src/main/java/com/example/daero/issue_list/di/viewModelModule.kt
@@ -1,11 +1,17 @@
 package com.example.daero.issue_list.di
 
+import com.example.daero.core.storage.AppStorage
+import com.example.daero.core.storage.AppStorageImpl
 import com.example.daero.issue_list.presentation.IssueListViewModel
 import com.example.daero.new_issue.NewIssueViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
+    single<AppStorage> {
+        AppStorageImpl(androidContext())
+    }
     viewModel {
         IssueListViewModel(get())
     }
