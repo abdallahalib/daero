6eb7502f39f0cd3763f59132fc3b7206d1f3059f
abdallahali <theabdallahali@gmail.com>
2026-06-03 23:55:18 +0300

    Add loading indicator to NewIssueScreen during photo capture

--- app/src/main/java/com/example/daero/new_issue/presenation/NewIssueScreen.kt
+++ app/src/main/java/com/example/daero/new_issue/presenation/NewIssueScreen.kt
@@ -2,7 +2,10 @@ package com.example.daero.new_issue.presenation
 
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -10,6 +13,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
@@ -51,5 +55,13 @@ fun NewIssueScreen(
             imageCapture = imageCapture,
             onCaptureClicked = { onIntent(NewIssueIntent.OnCapturePhotoClicked) },
         )
+        if (newIssueUiState.isLoading) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
     }
 }
