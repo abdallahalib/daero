5479149ed2c4a01fde1d3b649917c2863d8753f4
abdallahali <theabdallahali@gmail.com>
2026-06-03 23:44:49 +0300

    Add CaptureButton component for photo capture functionality

---
+++ app/src/main/java/com/example/daero/new_issue/components/CaptureButton.kt
@@ -0,0 +1,37 @@
+package com.example.daero.new_issue.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CaptureButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .size(64.dp)
+            .clip(CircleShape)
+            .background(Color.White)
+            .clickable(
+                onClick = {
+                    TODO("Not implemented")
+                }
+            )
+    )
+}
+
+@Preview
+@Composable
+fun CaptureButtonPreview() {
+    CaptureButton(onClick = {})
+}
