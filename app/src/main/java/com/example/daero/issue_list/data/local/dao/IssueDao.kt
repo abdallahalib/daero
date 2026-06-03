74787788822224ccfb7e3ac0e178c3c826b3ad77
abdallahali <theabdallahali@gmail.com>
2026-06-03 19:09:19 +0300

    Add IssueDao interface for issue data access

---
+++ app/src/main/java/com/example/daero/issue_list/data/local/dao/IssueDao.kt
@@ -0,0 +1,26 @@
+package com.example.daero.issue_list.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.daero.issue_list.data.local.entity.IssueEntity
+import com.example.daero.issue_list.domain.model.IssueSyncStatus
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface IssueDao {
+
+    @Query("SELECT * FROM issues")
+    fun loadAllIssues(): Flow<List<IssueEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insertIssue(issue: IssueEntity)
+
+    @Update
+    suspend fun updateIssue(issue: IssueEntity)
+
+    @Query("UPDATE issues SET sync_status = :syncStatus WHERE id = :id")
+    suspend fun updateIssueSyncStatus(id: String, syncStatus: IssueSyncStatus)
+}
