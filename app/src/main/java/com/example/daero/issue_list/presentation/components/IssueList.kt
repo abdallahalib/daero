fd361ed50800af5802c717d091e9e7d15591b41d
abdallahali <theabdallahali@gmail.com>
2026-06-03 21:54:42 +0300

    Add IssueList composable for displaying a list of issues

---
+++ app/src/main/java/com/example/daero/issue_list/presentation/components/IssueList.kt
@@ -0,0 +1,76 @@
+package com.example.daero.issue_list.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.daero.issue_list.presentation.model.IssueUi
+
+@Composable
+fun IssueList(
+    issues: List<IssueUi>,
+    onItemClick: (IssueUi) -> Unit,
+) {
+    if (issues.isEmpty()) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = androidx.compose.ui.Alignment.Center
+        ) {
+            Text(text = "No field notes yet.")
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
+            items(issues) {
+                IssueItem(
+                    issueUi = it,
+                    onCLick = { onItemClick(it) }
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun IssueListPreview() {
+    val issues = listOf(
+        IssueUi(
+            id = "1",
+            photoPath = "",
+            title = "Test Title 1",
+            notes = "Test Notes 1",
+            location = "Test Location 1",
+            priority = "High",
+            status = "Open",
+            syncStatus = "Synced",
+            createdAt = "2024-06-01 12:00:00",
+            updatedAt = "2024-06-01 12:00:00",
+        ),
+        IssueUi(
+            id = "2",
+            photoPath = "",
+            title = "Test Title 2",
+            notes = "Test Notes 2",
+            location = "Test Location 2",
+            priority = "Low",
+            status = "Closed",
+            syncStatus = "Not Synced",
+            createdAt = "2024-06-02 12:00:00",
+            updatedAt = "2024-06-02 12:00:00",
+        )
+    )
+    IssueList(
+        issues = issues,
+        onItemClick = {}
+    )
+}
+
