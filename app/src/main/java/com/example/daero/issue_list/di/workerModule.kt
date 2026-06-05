72ae220a07620318f639645605b7ace1464990de
abdallahali <theabdallahali@gmail.com>
2026-06-05 15:42:30 +0300

    Add WorkManager instance to workerModule for SyncWorker dependency injection

--- app/src/main/java/com/example/daero/issue_list/di/workerModule.kt
+++ app/src/main/java/com/example/daero/issue_list/di/workerModule.kt
@@ -1,9 +1,12 @@
 package com.example.daero.issue_list.di
 
+import androidx.work.WorkManager
 import com.example.daero.issue_list.data.worker.SyncWorker
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.workmanager.dsl.worker
 import org.koin.dsl.module
 
 val workerModule = module {
+    single<WorkManager> { WorkManager.getInstance(androidContext()) }
     worker { SyncWorker(get(), get()) }
 }
