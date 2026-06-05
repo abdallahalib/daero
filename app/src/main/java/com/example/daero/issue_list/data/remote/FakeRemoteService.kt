fb0a26c92fe9435a78ffa3a4ce762a30ad634611
abdallahali <theabdallahali@gmail.com>
2026-06-05 16:24:40 +0300

    Enhance issue creation to assign remoteId when synced and add logging

--- app/src/main/java/com/example/daero/issue_list/data/remote/FakeRemoteService.kt
+++ app/src/main/java/com/example/daero/issue_list/data/remote/FakeRemoteService.kt
@@ -1,24 +1,30 @@
 package com.example.daero.issue_list.data.remote
 
+import android.util.Log
 import com.example.daero.issue_list.domain.model.Issue
 import com.example.daero.issue_list.domain.model.IssueSyncStatus
 import com.example.daero.issue_list.domain.model.Result
 import com.example.daero.issue_list.domain.remote.RemoteService
 import kotlinx.coroutines.delay
+import java.util.UUID
 import kotlin.time.Duration.Companion.seconds
 
+private const val TAG = "FakeRemoteService"
 class FakeRemoteService : RemoteService {
 
     private val issueSyncStatus = IssueSyncStatus.SYNCED
 
     override suspend fun createIssue(issue: Issue): Issue {
+        Log.d(TAG, "Creating issue with id ${issue.id}")
         uploadPhoto(issue.id, issue.photoPath)
         return issue.copy(
-            syncStatus = issueSyncStatus
+            syncStatus = issueSyncStatus,
+            remoteId = if (issueSyncStatus == IssueSyncStatus.SYNCED) UUID.randomUUID().toString() else null
         )
     }
 
     override suspend fun updateIssue(issue: Issue): Issue {
+        Log.d(TAG, "Updating issue with id ${issue.id}")
         uploadPhoto(issue.id, issue.photoPath)
         return issue.copy(
             syncStatus = issueSyncStatus
