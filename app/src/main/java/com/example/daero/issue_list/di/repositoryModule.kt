1eb1e9eec4ac6d0a0f38aafac923bcd3edd21b20
abdallahali <theabdallahali@gmail.com>
2026-06-05 14:48:54 +0300

    Update IssueListRepositoryImpl instantiation to include additional dependency

--- app/src/main/java/com/example/daero/issue_list/di/repositoryModule.kt
+++ app/src/main/java/com/example/daero/issue_list/di/repositoryModule.kt
@@ -5,5 +5,5 @@ import com.example.daero.issue_list.domain.repository.IssueListRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single<IssueListRepository> { IssueListRepositoryImpl(get()) }
+    single<IssueListRepository> { IssueListRepositoryImpl(get(), get()) }
 }
